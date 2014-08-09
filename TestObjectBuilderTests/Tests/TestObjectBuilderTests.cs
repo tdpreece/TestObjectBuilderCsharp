af015162c5b7cf396473a21e19a4279320ef52d5
tdpreece <tdpreece@gmail.com>
2014-08-09 16:45:39 +0100

    Adding Test project and nunit.

---
+++ TestObjectBuilderTests/Tests/TestObjectBuilderTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+using TestObjectBuilder;
+
+namespace TestObjectBuilderTests.Tests
+{
+    public class TestObjectBuilderTests
+    {
+        [TestFixture]
+        public class With
+        {
+            [Test]
+            public void FirstTest()
+            {
+                Assert.AreEqual(1, 1);
+            }            
+        }
+    }
+}
