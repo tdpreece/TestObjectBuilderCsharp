ebe106d88f9787423594cf7fca31a999c23a8647
tdpreece <tdpreece@gmail.com>
2014-08-24 13:00:35 +0100

    Added a first simple unit test for TestObjectBuilderBuilder.

---
+++ TestObjectBuilderTests/TestClassesAndDependencies/ProductWithZeroArgumentConstructor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestObjectBuilderTests
+{
+    public class ProductWithZeroArgumentConstructor
+    {
+        public ProductWithZeroArgumentConstructor() { }
+
+        public IDependency1 FirstDependency { get; set; }
+    }
+}
---
+++ TestObjectBuilderTests/Tests/TestObjectBuilderBuilderTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+using TestObjectBuilder;
+
+namespace TestObjectBuilderTests.Tests
+{
+    public class TestObjectBuilderBuilderTests
+    {
+        [TestFixture]
+        public class CreateNewObject
+        {
+            [Test]
+            public void CreatesProductWithoutPropertiesAndAZeroArgConstructor()
+            {
+                // Arrange
+                ITestObjBuilder<ProductWithoutProperties> builder = 
+                    TestObjectBuilderBuilder<ProductWithoutProperties>.CreateNewObject();
+
+                // Act
+                ProductWithoutProperties product = builder.Build();
+
+                // Assert
+                Assert.NotNull(product);
+            }
+
+        }
+    }
+}
