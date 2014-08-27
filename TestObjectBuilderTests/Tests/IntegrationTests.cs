b6025316ceeab19f2d02897f324fd080f30a1c87
tdpreece <tdpreece@gmail.com>
2014-08-27 15:24:48 +0100

    Added an integration test for TestObjectBuilderBuilder and TestObjectBuilder.

---
+++ TestObjectBuilderTests/Tests/IntegrationTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+using TestObjectBuilder;
+
+namespace TestObjectBuilderTests.Tests
+{
+    [TestFixture]
+    class IntegrationTests
+    {
+        [Test]
+        public void BuildProductUsingSingleArgumentConstructorAndSettingOneAdditionalProperty()
+        {
+            // Arrange
+            TestObjectConstructorArgument constructorArg = 
+                new TestObjectConstructorArgument("ConstructorArg1", typeof(IDependency1));
+            TestObjectConstructorArgumentList constructorArguments =
+                new TestObjectConstructorArgumentList() { constructorArg };
+            Dependency1 inputDependency1Value = new Dependency1();
+            Dependency2 inputDependency2Value = new Dependency2();
+
+            // Act
+            ITestObjBuilder<Product> builder = TestObjectBuilderBuilder<Product>.CreateNewObject(
+                constructorArguments);
+            builder.With(ConstructorArg1 => inputDependency1Value);
+            builder.With(SecondDependency => inputDependency2Value);
+            Product product = builder.Build();
+
+            // Assert
+            Assert.AreEqual(inputDependency1Value, product.FirstDependency);
+            Assert.AreEqual(inputDependency2Value, product.SecondDependency);
+            Assert.AreEqual(null, product.ThirdDependency);
+        }
+    }
+}
