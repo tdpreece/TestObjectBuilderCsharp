c8dda5bbb1bc56787c8c0baf0bd96bc72e2c37c6
tdpreece <tdpreece@gmail.com>
2014-08-28 18:06:48 +0100

    Add ProductBuilderFromScratch.

---
+++ Examples/ProductBuilderFromScratch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Examples
+{
+    class ProductBuilderFromScratch
+    {
+        Product Build()
+        {
+            return new Product(this.X);
+        }
+
+        ProductBuilderFromScratch With(int x)
+        {
+            this.X = x;
+            return this;
+        }
+
+        ProductBuilderFromScratch With(List<int> y)
+        {
+            this.Y = y;
+            return this;
+        }
+
+        ProductBuilderFromScratch But()
+        {
+            return (ProductBuilderFromScratch)this.MemberwiseClone();
+        }
+
+        public int X { get; set; }
+        public List<int> Y { get; set; }
+    }
+}
